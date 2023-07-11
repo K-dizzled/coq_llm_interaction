667a2d1de9b3c9a4d77cb59d827018ce867973c8
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-07-11 19:03:45 +0200

    Initialize project with a submodule

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+sys.path.insert(1, 'coqpylspclient')
+
+from coqpylspclient.coqlspclient.proof_view import ProofView
+
+sys.path.pop()
+
+# Create an instance of a coq-lsp client and initialize it.
+file_path = os.path.join("coqpylspclient/tests/resources", "aux.v")
+pv = ProofView(file_path)
+
+print(pv.all_theorem_names())
+
+pv.exit()

c7a973ce1cb5dc813977c5cf603056ef90308445
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-08-22 19:48:17 +0200

    Fix private key issue

--- src/run_coqpilot.py
+++ src/run_coqpilot.py
@@ -7,13 +7,11 @@ import sys
 
 coq_file = sys.argv[1]
 root_dir = sys.argv[2]
+openai_api_key = sys.argv[3]
 
 llm_prompt = CoqPromptSolveAdmitted(coq_file, root_dir)
 
-dotenv.load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-llm_interface = GPT35(OPENAI_API_KEY)
+llm_interface = GPT35(openai_api_key)
 interactor = Interactor(llm_prompt, llm_interface, is_silent=True)
 
 interactor.run(shots=15)
